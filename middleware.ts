85b52c239f247d2abe77979b5c5c60cc5b67dc95
Increase <increasechris124@gmail.com>
2024-01-03 09:41:10 +0100

    fix

--- middleware.ts
+++ middleware.ts
@@ -3,13 +3,13 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(req: NextRequest, res: NextResponse) {
   const auth = req.cookies.get("auth");
 
-  // if (req.nextUrl.pathname.startsWith("/dashboard")) {
-  //   if (!auth) {
-  //     return NextResponse.redirect(
-  //       new URL(`/auth/signin?redirect=y&src=${req.url}`, req.url)
-  //     );
-  //   }
-  // }
+  if (req.nextUrl.pathname.startsWith("/dashboard")) {
+    if (!auth) {
+      return NextResponse.redirect(
+        new URL(`/auth/signin?redirect=y&src=${req.url}`, req.url)
+      );
+    }
+  }
 
   const checkedRoutes = [
     "/events",
